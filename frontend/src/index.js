18b7b87bab8bde26a5e9021bcba9ec6cf8a62531
SairajMN <sairaj.nadaf@gmail.com>
2025-12-12 20:59:59 +0530

    import error

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Dashboard from './dashboard.jsx';
+import './index.css';
+
 // Register service worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
@@ -10,11 +15,6 @@ if ('serviceWorker' in navigator) {
       });
   });
 }
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Dashboard from './dashboard.jsx';
-import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
